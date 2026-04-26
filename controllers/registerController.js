67f1754520940b7388818afbcd4f33efdda98cb3
Omar-55x <omar4cod3@gmail.com>
2026-04-26 16:04:46 +0300

    Modified the registerController to use mongoDB instead of JSON files and tested the register route

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,31 +1,23 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-};
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and Password are required' });
 
-    const duplicate = usersDB.users.find(person => person.username === user);
-    if (duplicate) return res.status(409).json({ 'message': `The user ${user} already exists` });       // Conflict
+    const duplicate = await User.findOne({ username: user }).exec();
+    if (duplicate) return res.sendStatus(409);       // Conflict
     
     try {
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        const newUser = {
-            "username": user,
-            "roles": {"user": 2001},
-            "password": hashedPwd
-        };
-        usersDB.setUsers([...usersDB.users, newUser]);
 
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
+        const result = await User.create({
+            "username": user,
+            "password": hashedPwd
+        });
+
+        console.log(result);
+        
         res.status(201).json({ 'message': `New user ${user} created` });
     } catch (err) {
         res.status(500).json({ 'message': err.message });
