3ec37d4794a13e7fe941ea6dc82b85c3635bd8aa
Omar-55x <omar4cod3@gmail.com>
2026-04-26 16:48:31 +0300

    Implemented and tested changes to use MongoDB for storing data instead of JSON files

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,28 +1,19 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data; }
-};
-const path = require('path');
-const fsPromises = require('fs').promises;
+const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(204);      // No content
     const refreshToken = cookies.jwt;
 
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
         return res.sendStatus(204);
     }
 
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = { ...foundUser, refreshToken: '' }
-    usersDB.setUsers([...otherUsers, currentUser]);
-    fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.js'),
-        JSON.stringify(usersDB.users)
-    )
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result);
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
